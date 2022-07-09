ef103caa4be7f06471720c4eded6b46d935ca1bf
hyun-an <hyunseok930@gmail.com>
2022-07-08 17:50:54 -0700

    bruh

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3001
 const app = express()
 app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
 app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/api', (req, res) => {
   res.json({ message: 'Hello from server!' })
