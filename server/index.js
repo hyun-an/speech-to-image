731754415293a6001b4bf7104c4fe67402189c63
ibepurplee <hyunseok930@gmail.com>
2022-07-10 10:41:57 -0700

    proxy update

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 const PORT = process.env.PORT || 3001
 
 const app = express()
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.resolve(__dirname, '../client/build')))
